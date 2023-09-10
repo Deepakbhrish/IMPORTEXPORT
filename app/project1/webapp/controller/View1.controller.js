6a7635791af82da05053011f6c278350787a9cbd
Deepak gupta <122858337+Deepakbhrish@users.noreply.github.com>
2023-09-10 22:01:28 +0530

    Update View1.controller.js

--- app/project1/webapp/controller/View1.controller.js
+++ app/project1/webapp/controller/View1.controller.js
@@ -1,18 +1,63 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/util/File",
+    "sap/ui/core/Fragment",
 
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,MessageBox) {
+    function (Controller,MessageBox,JSONModel,FileUtil, Fragment) {
         "use strict";
 
         return Controller.extend("project1.controller.View1", {
             onInit: function () {
+                var oData = {
+                    dataSet: []
+                  };
+                  var oModel = new JSONModel(oData);
+                  this.getView().setModel(oModel);
+          
+                  var script = document.createElement('script');
+                          script.onload = function () {
+                              //The library has fully loaded, and  can call functions in it now.
+                              console.log('Library loaded!');
+                          };
+                          script.src = '//unpkg.com/xlsx/dist/xlsx.full.min.js';
+                          document.head.appendChild(script);
 
             },
+            onFileUpload: function(oEvent) {
+                debugger;
+    
+                var oFileUploader = oEvent.getSource();
+                var oFile = oFileUploader.oFileUpload.files[0];
+                
+                // var sFileName = oFile.name;
+                // var sFileExtension = sFileName.split(".").pop();
+                
+                // if (sFileExtension.toLowerCase() !== "xlsx") {
+                //   MessageToast.show("Please upload an Excel file (XLSX).");
+                //   return;
+                // }
+                
+                var oReader = new FileReader();
+                oReader.onload = function(e) {
+                  var data = new Uint8Array(e.target.result);
+                  var workbook = XLSX.read(data, {type: 'array'});
+                  var sheetName = workbook.SheetNames[0];
+                  var worksheet = workbook.Sheets[sheetName];
+                  var jsonData = XLSX.utils.sheet_to_json(worksheet, {header: 1});
+                  var oModel = this.getView().getModel();
+                  
+                  oModel.setProperty("/dataSet", jsonData.slice(0)); // Skip the header row
+                  oModel.refresh(true);
+                }.bind(this);
+                
+                oReader.readAsArrayBuffer(oFile);
+              },
             onDownloadItem: function(oEvent) {
                 var SelectValue = this.getView().byId("SelectData").getSelectedKey();
                 
